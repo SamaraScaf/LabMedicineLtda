3a18378f2ef643f01b61234bdbe4a45e062ab87b
Samara <samarascaf@gmail.com>
2023-08-02 13:02:35 -0300

    Alteração Img

--- ProjetoModulo_02/src/Components/HomeComponents/CardInfo/CardInfo.jsx
+++ ProjetoModulo_02/src/Components/HomeComponents/CardInfo/CardInfo.jsx
@@ -5,7 +5,7 @@ function CardInfo({ titulo, valor }) {
         <div className="card-info">
             {/* <div className="card-body">                 */}
                 {/* <div className="d-flex"> */}
-                    <img src="src/assets/paciente.png"/>
+                    <img src="src/assets/paciente.png" className="mb-3" width="10" height="10"/>
                     <p className="valor-text">{valor}</p>
                 {/* </div>                 */}
 
